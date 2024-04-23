346f49d9901d700df65c416f1bba98488d282f28
DaniLuVi <d.luque@edu.uah.es>
2024-04-23 20:18:28 +0200

    no consigo enlazar las dos ventanas

---
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/TableroController.java
@@ -0,0 +1,28 @@
+package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableroController implements Initializable {
+
+    @FXML
+    protected void play() {
+
+    }
+    @FXML
+    protected void pause() {
+
+    }
+    @FXML
+    protected void guardarDatosPartida() {
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Se ejecuta el controlador del tablero.\n");
+
+    }
+}
