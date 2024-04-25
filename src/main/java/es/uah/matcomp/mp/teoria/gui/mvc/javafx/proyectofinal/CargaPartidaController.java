85ade5aeffa369557be6560af73c4623b984b080
DaniLuVi <d.luque@edu.uah.es>
2024-04-25 20:53:20 +0200

    seguir un poco más

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/CargaPartidaController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/CargaPartidaController.java
@@ -1,10 +1,19 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
 
+import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
-public class CargaPartidaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CargaPartidaController implements Initializable {
     private Stage scene;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Se ejecuta el controlador para la carga de partida.");
+
+    }
     public void setStage(Stage s) {
         this.scene = s;
     }
