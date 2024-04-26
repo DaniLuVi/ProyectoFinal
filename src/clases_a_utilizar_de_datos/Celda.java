e5da041773685ca80c197634447e5205d60420a0
d.luque <d.luque@edu.uah.es>
2024-04-26 12:26:04 +0200

    empezando a programar las clases del tablero

--- src/clases_a_utilizar_de_datos/Celda.java
+++ src/clases_a_utilizar_de_datos/Celda.java
@@ -1,4 +1,13 @@
 package clases_a_utilizar_de_datos;
 
-public class Celda<TipoDato> {
+import estructuras_de_datos_implementadas.listaDoblementeEnlazada.ListaDoblementeEnlazada;
+
+public class Celda {
+    private ListaDoblementeEnlazada<TipoDato> listaIndividuos;
+    private ListaDoblementeEnlazada<TipoDato> listaEntornos;
+    public Celda() {
+        this.listaIndividuos = new ListaDoblementeEnlazada<TipoDato>();
+        this.listaEntornos = new ListaDoblementeEnlazada<TipoDato>();
+    }
+
 }
