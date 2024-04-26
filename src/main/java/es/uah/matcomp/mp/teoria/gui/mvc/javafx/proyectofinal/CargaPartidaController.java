18ca5b00b8f22d6e45aa531879cc9f103868e17b
DaniLuVi <d.luque@edu.uah.es>
2024-04-27 01:23:11 +0200

    seguir un poco más

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/CargaPartidaController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/CargaPartidaController.java
@@ -1,6 +1,11 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -8,7 +13,32 @@ import java.util.ResourceBundle;
 
 public class CargaPartidaController implements Initializable {
     private Stage scene;
+    @FXML
+    private MenuButton partidas;
+    private int num_partidas;
+    public void aparecen_num_partidas_guardadas() {
+        for (int i = 0; i < num_partidas; i++) {
+            MenuItem partida_guardada = new MenuItem("Partida " + i);
+            partidas.getItems();
+        }
+    }
+    @FXML
+    protected void onCargaPartida() {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tablero-view.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 620, 460);
+            stage.setTitle("Tablero de juego");
+            stage.setScene(scene);
+            TableroController p = fxmlLoader.getController();
 
+            p.setStage(stage);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Se ejecuta el controlador para la carga de partida.");
