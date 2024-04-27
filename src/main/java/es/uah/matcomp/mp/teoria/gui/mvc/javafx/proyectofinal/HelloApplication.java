b84628b27492cefdd22caf361742fc73cb33ad96
DaniLuVi <d.luque@edu.uah.es>
2024-04-27 20:46:32 +0200

    seguir un poco más

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/HelloApplication.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/HelloApplication.java
@@ -4,17 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static final Logger log = LogManager.getLogger(HelloApplication.class);
     @Override
     public void start(Stage stage) throws IOException {
+
+        log.info("Arranque de inicio de la aplicación con su ventana principal(menú) de inicio.");
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 820, 640);
         stage.setTitle("Pantalla incial");
         stage.setScene(scene);
         stage.show();
+
+        log.info("El arranque de lal menú ha sido completado.");
     }
 
     public static void main(String[] args) {
