84e0ed455d5d401eec535cb97a3a0936affffc23
DaniLuVi <d.luque@edu.uah.es>
2024-04-28 13:44:09 +0200

    conseguido implementar ya bien el tablero, falta que cada casilla abra una nueva ventana, y creando la info de dentro de las casillas

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/ElementosCasillaController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/ElementosCasillaController.java
@@ -1,7 +1,9 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -9,10 +11,21 @@ import java.util.ResourceBundle;
 
 public class ElementosCasillaController implements Initializable {
     private Stage scene;
+    @FXML
+    private Label individuo1;
+    @FXML
+    private Label individuo2;
+    @FXML
+    private Label individuo3;
+    @FXML
+    private Label recurso1;
+    @FXML
+    private Label recurso2;
+    @FXML
+    private Label recurso3;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Se ejecuta el controlador de los elementos de una casilla.");
-
     }
     public void setStage(Stage s) {
         this.scene = s;
