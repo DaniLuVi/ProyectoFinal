7a6ff2e5b6f08bfe1c2a933b9b2f6151b392f4bd
DaniLuVi <d.luque@edu.uah.es>
2024-05-04 01:27:16 +0200

    mas o menos estoy empezando con el guardado de datos de la partida

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/PantallaCerrarTrasGuardado.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/PantallaCerrarTrasGuardado.java
@@ -1,14 +1,25 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class PantallaCerrarTrasGuardado {
     @FXML
     private Button si;
     @FXML
     private Button no;
+    private Stage stage;
 
-
+    @FXML
+    protected void ButtonSi() {
+        stage.close();
+        Platform.exit();
+    }
+    @FXML
+    protected void ButtonNo() {
+        stage.close();
+    }
 
 }
