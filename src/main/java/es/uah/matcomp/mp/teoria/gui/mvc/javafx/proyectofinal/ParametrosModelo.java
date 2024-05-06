30b929e1cb0792f105b8b9a2222b63c239bac8ac
DaniLuVi <d.luque@edu.uah.es>
2024-05-06 17:21:35 +0200

    cambiando el guardado de los datos al modelo de datos

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/ParametrosModelo.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/proyectofinal/ParametrosModelo.java
@@ -1,6 +1,11 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
 
 import clases_a_utilizar_de_datos.Individuo;
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ParametrosModelo {
     private int filas;
@@ -66,5 +71,24 @@ public class ParametrosModelo {
 
     public void setV(int v) {
         V = v;
+    }
+
+    public static <T> void guardarObjetoEnArchivo(String rutaArchivo, T objeto) {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(rutaArchivo)) {
+            gson.toJson(objeto, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // Método para cargar un objeto desde un archivo JSON
+    public static <T> T cargarObjetoDesdeArchivo(String rutaArchivo, Class<T> clase) {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(rutaArchivo)) {
+            return gson.fromJson(reader, clase);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
