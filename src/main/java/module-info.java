4a3355e10f6155dd49a1b2011432728583474db2
DaniLuVi <d.luque@edu.uah.es>
2024-05-20 18:57:20 +0200

    ya se hace un bucle de control entero bien (solo 1)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
@@ -10,4 +9,5 @@ module es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal {
 
     opens es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal to javafx.fxml;
     exports es.uah.matcomp.mp.teoria.gui.mvc.javafx.proyectofinal;
+
 }
